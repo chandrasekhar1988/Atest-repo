72cb8b5f2e49837d7b8eeb8e037b15fb6bf7a427
chandrasekhar1988 <chadrasekhar.maroju@gmail.com>
2026-04-06 11:37:10 +0530

    shell scripts

--- disk-usage.sh
+++ disk-usage.sh
@@ -16,6 +16,6 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "High Disk Usage" vasanthachandrasekhar@gmail.com
+#echo "$message" | mail -s "High Disk Usage" vasanthachandrasekhar@gmail.com
 
-#sh mail.sh "DevOps Team" "High Disk Usage" "$message" "chandrasekhar.maroju@gmail.com" "ALERT High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "vasanthachandrasekhar@gmail.com" "ALERT High Disk Usage"
